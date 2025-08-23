a732efc3067aa580f0ae907968bf7e6251ea4650
Akshada aware <akshadaaware8055@gmail.com>
2025-08-23 21:21:14 +0530

    Create day9.cpp
    longest common prefix

---
+++ day9.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+string longestCommonPrefix(vector<string> &strs) {
+    if (strs.empty()) return "";
+
+    string prefix = strs[0];
+    for (int i = 1; i < strs.size(); i++) {
+        int j = 0;
+        while (j < prefix.size() && j < strs[i].size() && prefix[j] == strs[i][j]) {
+            j++;
+        }
+        prefix = prefix.substr(0, j);
+        if (prefix.empty()) return "";
+    }
+    return prefix;
+}
+
+int main() {
+    int n;
+    cout << "Enter number of strings: ";
+    cin >> n;
+
+    vector<string> strs(n);
+    cout << "Enter strings: ";
+    for (int i = 0; i < n; i++) {
+        cin >> strs[i];
+    }
+
+    cout << "Longest Common Prefix = \"" << longestCommonPrefix(strs) << "\"\n";
+    return 0;
+}
