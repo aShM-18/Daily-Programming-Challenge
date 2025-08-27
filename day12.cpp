461bae6890a3d82ed135deddc132e955d6fd71d3
Akshada aware <akshadaaware8055@gmail.com>
2025-08-27 20:45:16 +0530

    Create day12.cpp
    valid parentheses with multiple types

---
+++ day12.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <stack>
+#include <string>
+using namespace std;
+
+bool isValid(string s) {
+    stack<char> st;
+    for (char c : s) {
+        if (c == '(' || c == '{' || c == '[') st.push(c);
+        else {
+            if (st.empty()) return false;
+            if (c == ')' && st.top() != '(') return false;
+            if (c == '}' && st.top() != '{') return false;
+            if (c == ']' && st.top() != '[') return false;
+            st.pop();
+        }
+    }
+    return st.empty();
+}
+
+int main() {
+    string s;
+    cout << "Enter string: ";
+    cin >> s;
+    cout << (isValid(s) ? "true" : "false") << endl;
+}
